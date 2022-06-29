e877b92dd2ef49c483011084cd1067ed317ecfc6
John Josephsen <jjosephsenyt@gmail.com>
2022-06-29 15:27:29 -0400

    styling changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <div className={styles.main}>
           <section className={styles.home_sec} id="Home">
             <h1 className={styles.heading}>John A Josephsen</h1>
-            <h2 className={styles.subheading}>An Upcoming Web Developer</h2>
+            <h2 className={styles.subheading}>A Web Developer</h2>
             <div className={styles.social_links}>
               <div className={styles.social_icon}>
                 <FontAwesomeIcon className={styles.fai} icon={faInstagram}/>
@@ -38,7 +38,6 @@ const Home: NextPage = () => {
               <div className={styles.about_text}>I am currently an undergraduate at Montclair State University in NJ studying for a BA in Computer Science. I enjoy designing and developing websites, both via Wordpress and/or custom code.</div>
               <div className={styles.about_image}>
                 <Image src="/images/file0.png" alt="oops." width={400} height={300}/>
-
               </div>
             </div>
           </section>         
